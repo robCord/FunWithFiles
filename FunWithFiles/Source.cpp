43fe7341de0e077536bd5b10d44f4c7d10c97ce3
ROBERTO CORDOVA <ROBCORDO@uat.edu>
2020-02-08 13:19:18 -0700

    final changes

--- FunWithFiles/Source.cpp
+++ FunWithFiles/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 //Author: Roberto Cordova
 //Date: 1/30/20
 //Purpose: transform data and then convert to a text file
@@ -8,64 +10,88 @@
 
 int main()
 {
-	//************************File Reader***************************
-	//
-	//
-	//var to hold user input
-	std::string stuffToRemember;
-	//intro
-	std::cout << "\n\n\n\t\tThe Ultimate File Construction\n\n";
-	std::cout << "\n\tThe ultimate remembering experience money can acquire\n";
-	//init file object
-	//create an object file handle to work around with
-	std::ofstream obj_file_handle;
-	//opening file
-	obj_file_handle.open("memory.txt");
-	//loop to write 5 statements
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout << "You have used " << i + 1 << " of blanks in memory to write to.\n";
-		std::cout << "\nWhat would you like to recall?\n";
-		//receive user input
-		std::getline(std::cin, stuffToRemember);
+	//sets array for CONST keywords 
+	const std::string KEYWORDS[] = {
+		"asm", "auto", "bool", "break", "case", "catch",
+		"char", "class", "const", "const_cast", "continue",
+		"default", "delete", "do", "double", "dynamic_cast",
+		"else", "enum", "explicit", "export", "extern",
+		"false", "float", "for", "friend", "goto", "if",
+		"inline", "int", "long", "mutable", "namespace", "new",
+		"operator", "private", "protected", "public", "register",
+		"reinterpret_cast", "return", "short", "signed", "sizeof",
+		"static", "static_cast", "struct", "switch", "template", 
+		"this", "throw", "true", "try", "typedef", "typeid", "typename",
+		"unison", "unsigned", "using", "virtual", "void", "volatile",
+		"wchar_t", "while"
+	};
+	//sets array for reserve words 
+	const std::string RESERVED_WORDS[] = {
+		"and", "and_eq", "bitand", "bitor", "compl", "not",
+		"not_eq", "or", "or_eq", "xor", "xor_eq"
+	};
 
-		//check to see if open before writing
-		if (obj_file_handle.is_open())
-		{
-			std::cout << "\nFile handle is open, currently writing to it!\n";
-			//write to file
-			obj_file_handle << stuffToRemember << std::endl;
-		}
-		else
-		{
-			std::cout << "\nError file handler is close, cannot write to file!\n";
+	//sets in file handler 
+	std::ifstream in_file_handler;
+	std::vector<std::string> file_text;
+
+	//opens file 
+	in_file_handler.open("CPP_Reference.txt");
+	//checks if open
+	if (in_file_handler.is_open()) 
+	{
+		//grabs string line
+		std::string line;
+		while (std::getline(in_file_handler, line)) 
+		{//sets string to lowercase 
+			std::transform(line.begin(), line.end(), line.begin(), ::tolower);
+			file_text.push_back(line);
 		}
 	}
-	//close file
-	obj_file_handle.close();
-	std::cout << "\nThe file has been closed!\n\n\n";
-
-	//***************************************************************
-		
-	//read the memory
-	//filestream variable file
-	std::fstream file;
-	std::string word, filename;
-
-	//filename of the file
-	filename = "memory.txt";
-
-	//opening file
-	file.open(filename.c_str());
-
-	//extracting words from the file
-	while(file >> word)
+	//creating a output file handler for keywords
+	std::ofstream out_file_handler("list.txt");
+	//creating a vector for keywords using the const 
+	std::vector<std::string> keywords;
+	//loop through defined keywords
+	for (std::string keyword : KEYWORDS)
 	{
-		//displayinf content
-		std::cout << word << std::endl;
-		
+		//grabs each line of the input file
+		for (std::string line : file_text)
+		{
+			//grabs the first word of each line 
+			std::string word = line.substr(0, line.find(" "));
+			//does word match current keyword
+			if (word == keyword)
+				//if it does it will be added to the list of keywords
+				keywords.push_back(line);
+		}
 	}
+	//outputs keywords to the output file
+	out_file_handler << "C++ Keywords Defined = " << keywords.size() << ":\n";
+	for (std::string line : keywords)
+		out_file_handler << line << "\n";
+
+	std::vector<std::string> reserved;
+
+	for (std::string reserve : RESERVED_WORDS)
+	{
+		//grabs each line of the input file
+		for (std::string line : file_text)
+		{
+			//grabs the first word of each line 
+			std::string word = line.substr(0, line.find(" "));
+			//does word match current keyword
+			if (word == reserve)
+				//if it does it will be added to the list of keywords
+				reserved.push_back(line);
+		}
+	}
+	// outputs the reserved to the output file
+	out_file_handler << "\nC++ Reserved words Defined = " << reserved.size() << ":\n";
+	for (std::string line : reserved)
+		out_file_handler << line << "\n";
 
 
-	
+	//closes file 
+	out_file_handler.close();
 }
